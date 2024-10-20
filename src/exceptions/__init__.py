234c4ede4c3aec46fe78332e2a4beddf9a84c4eb
madfrr <gs_ferreira@id.uff.br>
2024-10-20 17:03:40 -0300

    feat: exceptions

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import status
+from typing import List
 
 
 class BusinessException(Exception):
@@ -26,7 +27,19 @@ class TransactionDoesNotExist(BusinessException):
     def __init__(self, message="Transaction does not exist."):
         super().__init__(message=message, status_code=status.HTTP_404_NOT_FOUND)
 
+class InvalidTransaction(BusinessException):
+    def __init__(self, message="Invalid transaction. Buyer and Seller must be different"):
+        super().__init__(message=message, status_code=status.HTTP_400_BAD_REQUEST)
+
 class OrderDoesNotExist(BusinessException):
     def __init__(self, message="Order does not exist."):
         super().__init__(message=message, status_code=status.HTTP_404_NOT_FOUND)
 
+class InvalidImageType(BusinessException):
+    def __init__(self, content_type: str, ALLOWED_IMAGE_TYPES: List[str]):
+        message = f"Invalid image type. Type: {content_type}. Valid Types: {', '.join(ALLOWED_IMAGE_TYPES)}"
+        super().__init__(message=message, status_code=status.HTTP_400_BAD_REQUEST)
+
+class FileTooBig(BusinessException):
+    def __init__(self, message="File too big. File size must be under 5MB"):
+        super().__init__(message=message, status_code=status.HTTP_400_BAD_REQUEST)
