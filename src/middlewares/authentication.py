6b1667b08b11ab6ff72aff37df9a97318e9e5fc1
madfrr <gs_ferreira@id.uff.br>
2024-10-20 21:03:35 -0300

    feat: installing pre-commit

--- src/middlewares/authentication.py
+++ src/middlewares/authentication.py
@@ -2,20 +2,15 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.authentication import AuthenticationBackend, AuthCredentials, SimpleUser
 from fastapi import HTTPException
 from config import AppConfig
-from utils.logger import logger
+
 
 class BasicAuthBackend(AuthenticationBackend):
-
     def free_paths(self, path):
-        paths = [
-            f"{AppConfig.app_prefix}/health-check",
-            f"{AppConfig.app_prefix}/docs",
-            "/openapi.json"
-        ]
+        paths = [f"{AppConfig.app_prefix}/health-check", f"{AppConfig.app_prefix}/docs", "/openapi.json"]
         for free_path in paths:
             if free_path in path:
                 return True
-            
+
         return False
 
     async def authenticate(self, conn):
